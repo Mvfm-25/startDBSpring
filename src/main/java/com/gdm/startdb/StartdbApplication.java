6c1c7726513f2e74b43381355da0285687b76582
Mvfm <mvfm.filho@hotmail.com>
2025-11-07 10:54:56 -0300

    Adição de arquivos 'Spring' & 'Maven'.

---
+++ src/main/java/com/gdm/startdb/StartdbApplication.java
@@ -0,0 +1,13 @@
+package com.gdm.startdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdm/startdb/StartdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdm.startdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
