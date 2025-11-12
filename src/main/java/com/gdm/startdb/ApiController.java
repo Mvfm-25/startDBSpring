095b80e23e1a5362dc6075b75f269e1f30260ba8
Mvfm <mvfm.filho@hotmail.com>
2025-11-12 14:32:08 -0300

    Incorporação do 'Thymeleaf' no projeto para brincar com HTML & CSS. Split de 'Controller.java' para 'ViewController.java' & 'ApiController.java'.

--- src/main/java/com/gdm/startdb/ApiController.java
+++ src/main/java/com/gdm/startdb/ApiController.java
@@ -1,12 +1,14 @@
-// Simples controller para testes iniciais.
+// Parte do REST API.
 // [mvfm]
 
-// Criado : 07/11/2025  || Última vez alterado : 07/11/2025
+// Criado : 12/11/2025  || Última vez alterado : 12/11/2025
 
 package com.gdm.startdb;
 
 import java.util.*;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties.Tomcat.Mbeanregistry;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,60 +16,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 @RestController
-public class Controller {
+public class ApiController {
 
     private Set<Membros> nicers = new HashSet<>();
-
-    public Controller() {
+    public ApiController() {
         nicers = new HashSet<Membros>();
 
         nicers.add(new Membros("Mvfm", "Nicest"));
         nicers.add(new Membros("Kappe", "Nicer"));
         nicers.add(new Membros("Sheedoky", "Nicer"));
     }
-    
-
-    int hits = 0;
-
-    @GetMapping("")
-    @CrossOrigin(origins = "*")
-    public String home() {
-        
-    return """
-        <html>
-            <body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
-                <h1>G.D.M. IS ONLINE</h1>    
-                <button type='button' onclick="window.location.href='/gdm'">
-                    G.D.M.
-                </button>
-            </body>
-        </html>
-    """;
-    }
-
-    @GetMapping("/gdm")
-    public String gdm(){
-        if(hits == 5){
-            hits = 0;
-            return "G.D.M. - Always improving!";
-        }
-        
-        hits++;
-        return "G.D.M. - Being nice since 2018!";
-    }
 
     @GetMapping("/nice")
     public Set<Membros> nice(){
         return nicers;
     }
-    
+
     @PostMapping("/addnicer")
     public String addNicer(@RequestBody List<Membros> novosNicers){
         nicers.addAll(novosNicers);
         return "Nice(s) adicionado com sucesso!";
     }
-    
-    
 }
---
+++ src/main/java/com/gdm/startdb/ViewController.java
@@ -0,0 +1,46 @@
+// Simples controller para testes iniciais.
+// [mvfm]
+
+// Criado : 07/11/2025  || Última vez alterado : 12/11/2025
+
+package com.gdm.startdb;
+
+import java.util.*;
+
+import org.springframework.boot.autoconfigure.web.ServerProperties.Tomcat.Mbeanregistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+
+
+// Parte visual, HTML com Thymeleaf.
+@Controller
+public class ViewController {
+
+    private int hits = 0;
+
+    @GetMapping("")
+    public String home(Model modelo){
+        modelo.addAttribute("message", "G.D.M. IS ONLINE");
+        return "home";
+    }
+
+    @GetMapping("/gdm")
+    public String gdm(Model modelo){
+        if(hits == 5){
+            hits = 0;
+            modelo.addAttribute("message", "G.D.M. - Always improving!");
+        } else {
+            hits++;
+            modelo.addAttribute("message", "G.D.M. - Being nice since 2018!");
+        }
+        return "home";
+    }
+    
+}
